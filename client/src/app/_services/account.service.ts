290c96f13dd9fe1796641c55887acb0301fda0d4
Diego Dorrejo <dorrejo14@gmail.com>
2024-10-05 23:54:28 -0400

    end of section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,18 +4,17 @@ import { User } from '../_models/user';
 import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-
   private http = inject(HttpClient);
   baseUrl = 'https://localhost:5001/api/';
   currentUser = signal<User | null>(null);
 
-  login(model: any){
+  login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map(user => {
-        if(user){
+      map((user) => {
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
@@ -23,7 +22,19 @@ export class AccountService {
     );
   }
 
-  logout(){
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    );
+  }
+
+  logout() {
     localStorage.removeItem('user');
     this.currentUser.set(null);
   }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  input,
+  output,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
-@Component({ 
+@Component({
   selector: 'app-register',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  model: any = {}
+  model: any = {};
+  cancelRegister = output<boolean>();
+  private accountService = inject(AccountService);
 
-  register(){
-    console.log(this.model);
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.log(error),
+    });
   }
 
-  cancel(){
-    console.log("Cancelled");
+  cancel() {
+    this.cancelRegister.emit(false);
   }
 }
